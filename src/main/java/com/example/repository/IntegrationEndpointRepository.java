46c8f9cd787d47f14d802a99babf16a3ed7dd41f
Naresh2056 <Singam.Naresh@cognizant.com>
2026-04-27 23:07:01 +0530

    Delete src/main/java/com/example directory

--- src/main/java/com/example/repository/IntegrationEndpointRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.IntegrationEndpoint;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IntegrationEndpointRepository
-        extends JpaRepository<IntegrationEndpoint, Long> {
-
-}
--- src/main/java/com/example/repository/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository
-        extends JpaRepository<Notification, Long> {
-
-}
