852ae37ae0ddc1e86088edd05e70e01c02e293c0
Nakirakanti-Archana <Nakirakanti.Archana@cognizant.com>
2026-04-28 15:05:59 +0530

    Delete Retail-Q directory

--- Retail-Q/src/main/java/com/example/repository/ForecastRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.Forecast;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ForecastRepository extends JpaRepository<Forecast, Long> {
-
-    Forecast findByProduct_SkuAndLocation_LocationId(String sku, Long locationId);
-}
